141d64741a9e53d3566e5c8dfc9acf9e7edde999
lucciano01 <lucciano01@hotmail.com>
2025-04-07 17:53:40 -0300

    criando projeto

---
+++ teste_jenkins/src/main/java/br/edu/catolica/teste_jenkins/TesteJenkinsApplication.java
@@ -0,0 +1,13 @@
+package br.edu.catolica.teste_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteJenkinsApplication.class, args);
+	}
+
+}
---
+++ teste_jenkins/src/test/java/br/edu/catolica/teste_jenkins/TesteJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.catolica.teste_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
