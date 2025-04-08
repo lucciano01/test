15b308e5a76cceeaec6bd3a82296294a7826668b
lucciano01 <lucciano01@hotmail.com>
2025-04-08 09:44:04 -0300

    configuracao do swagger

---
+++ src/main/java/br/edu/catolica/teste_jenkins/controller/HelloController.java
@@ -0,0 +1,15 @@
+package br.edu.catolica.teste_jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/{name}")
+    public String helloUser(@PathVariable("name") String name){
+        return "Seja bem vindo " +name;
+    }
+}
