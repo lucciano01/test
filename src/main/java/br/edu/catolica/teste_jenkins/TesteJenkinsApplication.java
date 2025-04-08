12c590f62b304f82f8159be01e35c85a596a936f
lucciano01 <lucciano01@hotmail.com>
2025-04-08 10:48:48 -0300

    correcao no path do arquivo principal

--- src/main/java/br/edu/catolica/teste_jenkins/TesteJenkinsApplication.java
+++ src/main/java/br/edu/catolica/teste_jenkins/TesteJenkinsApplication.java
@@ -2,9 +2,16 @@ package br.edu.catolica.teste_jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TesteJenkinsApplication {
+public class TesteJenkinsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TesteJenkinsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TesteJenkinsApplication.class, args);
