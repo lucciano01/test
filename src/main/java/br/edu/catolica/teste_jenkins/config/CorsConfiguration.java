9977b74e1caa4beff10c29c731af61c0cb11c189
lucciano01 <lucciano01@hotmail.com>
2025-04-09 10:54:47 -0300

    configurando cors e corrigindo pom.xml

--- src/main/java/br/edu/catolica/teste_jenkins/config/CorsConfiguration.java
+++ src/main/java/br/edu/catolica/teste_jenkins/config/CorsConfiguration.java
@@ -9,7 +9,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-       registry.addMapping("/**")
-               .allowedMethods("GET", "POST", "PUT","DELETE");
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
